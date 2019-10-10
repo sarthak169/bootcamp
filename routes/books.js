ac9d2cf31da846292b0ac65dc5291b891305090b
sarthak169 <sarthakjain169@gmail.com>
2019-10-10 16:53:49 +0530

    day 3

--- routes/books.js
+++ routes/books.js
@@ -5,4 +5,32 @@ var router = express.Router();
 router.get('/search', function (req, res, next) {
   res.status(200).json({ id: 1, name: "Harry Potter", author: "J.K. Rowling" })
 });
+router.post('/add',function(req,res,next){
+  console.log(req.body);
+  res.status(200).json({message: ' Book saved successulg'});
+});
+router.get('/add', function(req,res,next){
+  // console.log(req.body);
+  res.render('book');
+});
+router.post('/add', function(req,res,next){
+  console.log(req.body);
+  res.json('Book has been posted');
+});
+router.get('/add/:id/:name/:auth', function(req,res,next){
+  const id = req.params.id 
+  const name=req.params.name 
+  const auth = req.params.auth
+  console.info('Name:',name,'id: ',id,'auth: ',auth);
+  res.status(200).json({Message:'Task completed'});
+});
+
+
+router.get('/find/:id', function(req,res,next){
+  const {id}=req.params;
+  console.info('here is the id you have entered',id);
+  res.status(200).json({message: 'Book has been found here'});
+});
+
+
 module.exports = router;
